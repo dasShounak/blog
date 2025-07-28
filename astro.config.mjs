e920fbf1006dc7652bffc0d1221db39368dffa53
Shounak Das <shounakdas@protonmail.com>
2025-07-28 17:38:51 +0000

    chore: Update @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://shounakdas.netlify.app/",
+  site: "https://shounakdas.org/",
   integrations: [tailwind({
     applyBaseStyles: true
   }), sitemap()]
